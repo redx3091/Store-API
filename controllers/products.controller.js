b67580fa80c79985c3555496c1e6e7e4014acfc7
Christian A <christiana@iMac-de-Christian.local>
2022-09-29 15:37:22 -0500

    ADD methods to get all products and populated the DB with some data

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,6 +1,12 @@
+const Product = require('../models/products.model');
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error('testing async errors');
-  res.status(200).json({ msg: 'products testing route' });
+  await Product.find({ name: 'vase table' })
+    .then((products) =>
+      res.status(200).json({ products, nbHits: products.length })
+    )
+    .catch((err) => res.status(500).json(err));
+  //res.status(200).json({ products });
 };
 const getAllProducts = async (req, res) => {
   res.status(200).json({ msg: 'products  route' });
--- models/products.model.js
+++ models/products.model.js
@@ -25,7 +25,7 @@ const productSchema = new mongoose.Schema({
   company: {
     type: String,
     enum: {
-      values: ['ikea', 'lddy', 'caressa', 'marcos'],
+      values: ['ikea', 'liddy', 'caressa', 'marcos'],
       message: '{VALUE} is not supported',
     },
     //enum: ['ikea', 'lddy', 'caressa', 'marcos'],
