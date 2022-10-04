8c8993fb1ad9fde08aa02f51fc494937a6b8f699
Christian A <christiana@iMac-de-Christian.local>
2022-10-04 12:06:43 -0500

    ADD option Select and Field to filter

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -2,7 +2,7 @@ const Product = require('../models/products.model');
 
 const getAllProductsStatic = async (req, res) => {
   await Product.find({})
-    .sort('-name price')
+    .select('name price')
     .then((products) =>
       res.status(200).json({ products, nbHits: products.length })
     )
@@ -10,7 +10,7 @@ const getAllProductsStatic = async (req, res) => {
   //res.status(200).json({ products });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -22,13 +22,18 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
-  console.log(queryObject);
+  //*console.log(queryObject);
   let result = Product.find(queryObject);
+  //* sort
   if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
     result = result.sort('createAt');
+  }
+  if (fields) {
+    const fieldList = fields.split(',').join(' ');
+    result = result.select(fieldList);
   }
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
